e8605b909f1b37cd10b98ddd5c4d16bd8fc93779
Serena Lea <serenelunation@gmail.com>
2019-09-04 01:41:10 -0400

    Finished Tutorial!!

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,58 @@
 <template>
-  <div>
-    <h1>Hello world!
+  <main>
+    <div class="background">
+      <app-masthead></app-masthead>
+    </div>
+    <div class="headings">
+      <h1>
+        Hello world!
         <br>I've gone through hell and back to create this page.
-    </h1>
-  </div>
+      </h1>
+      <p>Please call my therapist.</p>
+    </div>
+    <div class="links">Do not follow me on my social medias</div>
+    <div class="nextpage">Projects ></div>
+  </main>
 </template>
 
 <script>
+  import AppMastHead from '~/components/AppMastHead.vue'
+
   export default {
-    
+    components: {
+      AppMastHead
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+  main {
+    width: 100vw;
+    height: calc(100vh - 100px);
+    display: grid;
+    grid-template-columns: 100px 100px 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 50px 100px;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
 
+    .background {
+      grid-area: 1 / 3 / 5 / 6;
+    }
+
+    .headings {
+      grid-area: 2 / 2 / 3 / 6;
+    }
+
+    .links {
+      padding: 30px;
+      grid-area: 5 / 1 / 6 / 5;
+    }
+
+    .nextpage {
+      background: #eee;
+      color: black;
+      padding: 30px;
+      grid-area: 4 / 5 / 6 / 6;
+    }
+  }
 </style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,67 @@
 <template>
-  <div>
-    <h1>
-      Projects
-    </h1>
-  </div>
+  <main>
+    <section>
+      <h1>Projects</h1>
+      <div v-for="project in githubProjects" :key="project.id" class="projects">
+        <h3><a :href="project.html_url">{{ project.name }}</a></h3>
+        <p class="stars">🟐 {{ project.stargazers_count }}</p>
+        <p>{{ project.homepage }}</p>
+        <p>{{ project.description }}</p>
+      </div>
+    </section>
+    <aside>
+      <h3>About Me</h3>
+      <p>I am a stranger on the internet, there isn't much to know about me besides my ability to follow basic tutorials online.</p>
+    </aside>
+  </main>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   export default {
-    
+    computed: {
+      ...mapState([
+        "githubProjects",
+      ])
+    },
+    mounted() {
+      this.$store.dispatch("getGithubProjects")
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  main {
+    width: 80vw;
+    margin: 2% 8%;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 50px;
+    grid-row-gap: 0px;
 
+    section {
+      background: white;
+      color: black;
+      padding: 30px;
+      grid-area: 1 / 1 / 2 / 2;
+
+      a {
+        color: black
+      }
+    }
+
+    aside {
+      grid-area: 1 / 2 / 2 / 3;
+    }
+  }
+
+  .stars {
+    color: orangered;
+  }
+
+  .project {
+    margin-bottom: 30px;
+  }
 </style>
