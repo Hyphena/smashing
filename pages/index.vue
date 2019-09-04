650584a1db7805e020a09c3504de44ff11a90b77
Serena Lea <serenelunation@gmail.com>
2019-09-04 00:10:12 -0400

    Initial Commit!

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello world!
+        <br>I've gone through hell and back to create this page.
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
